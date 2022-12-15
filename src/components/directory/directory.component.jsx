865f376fcfe343332686192500e1f3ace554cd55
Lachlan Whelan <lachlan.whelan1@gmail.com>
2022-12-15 21:03:54 +1100

    fixed up home navigation routes and add redirect for netlify

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -6,31 +6,31 @@ const categories = [
       "id": 1,
       "title": "hats",
       "imageUrl": "https://i.ibb.co/cvpntL1/hats.png",
-      "routes" : "shop/hats"
+      "route" : "shop/hats"
     },
     {
       "id": 2,
       "title": "jackets",
       "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png",
-      "routes" : "shop/jackets"
+      "route" : "shop/jackets"
     },
     {
       "id": 3,
       "title": "sneakers",
       "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png",
-      "routes" : "shop/sneakers"
+      "route" : "shop/sneakers"
     },
     {
       "id": 4,
       "title": "womens",
       "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png",
-      "routes" : "shop/womens"
+      "route" : "shop/womens"
     },
     {
       "id": 5,
       "title": "mens",
       "imageUrl": "https://i.ibb.co/R70vBrQ/men.png",
-      "routes" : "shop/mens"
+      "route" : "shop/mens"
     }
 ]
 
